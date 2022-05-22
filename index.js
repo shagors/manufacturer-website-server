faf2784ad193d6ccd0a6fba04cc1531ae535362e
shagors <shajjadshagor@gmail.com>
2022-05-22 14:07:21 +0600

    data send from db to client side and data get

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vs0kl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+ async function run(){
+
+    try{
+        await client.connect();
+        const productCollection = client.db('manufacture_Co').collection('products');
+
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query).project(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(console.dir)
+
+
+
 app.get('/', (req, res) => {
     res.send('Server Connect from Manufacture')
 })
