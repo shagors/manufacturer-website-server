d239c58265a08ea32be356bdb4bf24f7496dc895
shagors <shajjadshagor@gmail.com>
2022-05-23 23:51:05 +0600

    make admin and set role on db and verify admin

--- index.js
+++ index.js
@@ -46,6 +46,21 @@ function verifyJWT(req, res, next){
             res.send(products);
         });
 
+        app.get('/user', verifyJWT, async(req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        });
+
+        app.put('/user/admin/:email', verifyJWT, async(req, res) => {
+            const email = req.params.email;
+            const filter = {email: email};
+            const updateDoc = {
+                $set: {role: 'admin'},
+            };
+            const result= await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        });
+
         app.put('/user/:email', async(req, res) => {
             const email = req.params.email;
             const user = req.body;
